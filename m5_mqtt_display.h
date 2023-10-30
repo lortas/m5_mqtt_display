8945ce10f3c30bbcff097a0825a6de2c29e103d8
lortas <lortas@users.noreply.github.com>
2023-10-31 00:52:11 +0100

    Create m5_mqtt_display.h

---
+++ m5_mqtt_display.h
@@ -0,0 +1,7 @@
+const char* broker  = "CHANGE_ME_MQTT_IP_ADDRESS";
+const int   b_port  = 1883;
+const char* b_user  = "CHANGE_ME_MQTT_USER";
+const char* b_pass  = "CHANGE_ME_MQTT_PASSWORD";
+const char* b_topic = "CHANGE_ME_MQTT_TOPIC";
+const char* ssid    = "CHANGE_ME_WIFI_SSID";
+const char* pass    = "CHANGE_ME_WIFI_PASSWORD";
